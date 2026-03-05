19b3ccb2b02a7ca15c108ec75a117c96179443ce
Nathan <lyxnnfp@gmail.com>
2026-03-05 18:16:05 +0100

    Primer commit

---
+++ src/test/java/com/proyectoFoc/proyectoFoc/ProyectoFocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoFoc.proyectoFoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
