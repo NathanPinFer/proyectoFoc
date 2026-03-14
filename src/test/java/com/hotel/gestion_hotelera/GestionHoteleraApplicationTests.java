e0c3688c410631f889a24ec12f1b7ad9289f01dd
alvarobellido25 <abbalvaro@hotmail.com>
2026-03-14 12:59:46 +0100

    Setup inicial: Spring Boot + conexión BD hotel_db

---
+++ src/test/java/com/hotel/gestion_hotelera/GestionHoteleraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.gestion_hotelera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionHoteleraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
